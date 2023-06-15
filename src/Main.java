f9265cd3b4bc1a2583526b154c0885c0a91f1729
Brodyaga05 <ulmart035@gmail.com>
2023-06-15 07:57:09 +0300

    break

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.print("Hello and welcome!");
-
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("Hello world");
-        }
+    int i = 0;
+        while (true){
+            if (i==15){
+                break;
+            }
+            System.out.println(i);
+            i++;
+    }
+        System.out.println("Мы вышли из цикла");
     }
 }
