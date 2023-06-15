9881f712a2951605e3148edb9119a96acc66ce04
Brodyaga05 <ulmart035@gmail.com>
2023-06-15 08:37:58 +0300

    continue

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-    int i = 0;
-        while (true){
-            if (i==15){
-                break;
-            }
-            System.out.println(i);
-            i++;
+    for(int i = 0; i<=15; i++){
+        if (i%2==0){
+            continue;
+        }
+        System.out.println("Это нечетное число " + i);
+
     }
-        System.out.println("Мы вышли из цикла");
-    }
+
+        }
 }
