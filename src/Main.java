cc5dcf2374fdf963445657b89d3c3d4095c620c1
Brodyaga05 <ulmart035@gmail.com>
2023-06-18 17:44:28 +0300

    for array

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,11 @@ public class Main {
        for (int i = 0; i<numbers.length; i++){
            System.out.println(numbers[i]);
        }
+        System.out.println();
+       int [] numbers1 = {1,2,3,4,5};
+       for(int i = 0; i< numbers1.length; i++){
+           System.out.println(numbers1[i]);
+       }
 
 
 
