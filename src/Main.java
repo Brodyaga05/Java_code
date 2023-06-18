f98f9d0ec296690bccb3e72dcf36e14de62857e7
Brodyaga05 <ulmart035@gmail.com>
2023-06-18 16:23:23 +0300

    inicilization

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int number = 10; // примитивный тип данных
         int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
 
 
 
