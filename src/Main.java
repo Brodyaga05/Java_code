970c2bb240f4b317848ddbe9a3f1c5fe14be101a
Brodyaga05 <ulmart035@gmail.com>
2023-06-18 15:51:40 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите возраст");
-        String age = scanner.nextLine();
-        switch (age){
-            case "нуль":
-                System.out.println("Вы родился");
-                break;
-            case "пять":
-                System.out.println("Ты пошел в садик");
-                break;
-            case "восемнадцать":
-                System.out.println("Вы пошел работать");
-                break;
-            default:
-                System.out.println("Ты не ты");
+        int number = 10; // примитивный тип данных
+        char character = 'a'; // примитивный тип данных
+        String s = "Hello";
+        String s1 = new String("Hello");
 
-        }
 
 
     }
