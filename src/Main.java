504bb0076441243c090b07686e3e6223640acba9
Brodyaga05 <ulmart035@gmail.com>
2023-06-18 16:10:47 +0300

    array

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int number = 10; // примитивный тип данных
-        char character = 'a'; // примитивный тип данных
-        String s = "Hello";
-        String s1 = new String("Hello");
+        int[] numbers = new int[5];
+
+
+
 
 
 
