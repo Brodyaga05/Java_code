1f08ed54f53bedc95cac588208789c6fb48122c0
Brodyaga05 <ulmart035@gmail.com>
2023-06-18 17:41:46 +0300

    for array

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         int[] numbers = new int[5];
        for(int i = 0; i<numbers.length; i++){
            numbers[i] = i*10;
-
-
-
+       }
+       for (int i = 0; i<numbers.length; i++){
+           System.out.println(numbers[i]);
        }
 
 
