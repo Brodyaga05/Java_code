cb271ea70d7566cd66cdfea0615a4cced9567ccd
Brodyaga05 <ulmart035@gmail.com>
2023-06-19 07:28:00 +0300

    string array

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-      int[] numbers = new int[5];
+      /*int[] numbers = new int[5];
       numbers[0] = 10;
         System.out.println(numbers[0]);
-        System.out.println(numbers[1]);
+        System.out.println(numbers[1]);*/
+        String[] strings = new String[3];
+        strings[0] = "Привет";
+        strings[1] = "Пока";
+        strings[2] = "Java";
+        System.out.println(strings[0]);
+
+
+
 
 
 
