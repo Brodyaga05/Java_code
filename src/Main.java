164b0f352474f48c81367fde2c4c5178beea384a
Brodyaga05 <ulmart035@gmail.com>
2023-06-20 20:57:21 +0300

    array one

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-      /*int[] numbers = new int[5];
-      numbers[0] = 10;
-        System.out.println(numbers[0]);
+     int [] numbers = {1,2,3};
         System.out.println(numbers[1]);
-        String[] strings = new String[3];
-        strings[0] = "Привет";
-        strings[1] = "Пока";
-        strings[2] = "Java";
-        for(int i=0; i<strings.length; i++){
-            System.out.println(strings[i]);
-        }
-        System.out.println();
-        for(String string:strings){
-            System.out.println(string);
-        }*/
-        int[] number1 = {1,2,3};
-        int sum = 0;
-        for(int x:number1){
-            sum = sum +x;
-        }
-        System.out.println();
-        System.out.println(sum);
-
-        int value = 0;
-        String s = null;
-        String s1 = "geretgrte";
 
 
 
