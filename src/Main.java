d1f6d11693abb2cedf40f9ce3c8ced6821f3ce9b
Brodyaga05 <ulmart035@gmail.com>
2023-06-20 21:06:50 +0300

    array one

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-     int [] numbers = {1,2,3};
-        System.out.println(numbers[1]);
+    /* int [] numbers = {1,2,3};
+        System.out.println(numbers[1]);*/
+        int [][] matrice = {{1,2,3},
+                            {32,4,5},
+                             {6,7,8}};
+        System.out.println(matrice[2][2]);
+
 
 
 
