9f10d0825fb05d7dbe5434923399a3b26a7e22f2
Brodyaga05 <ulmart035@gmail.com>
2023-06-21 20:33:05 +0300

    class

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,4 +1,14 @@
 public class ClassesAndObjects {
-}
+    Person person1 = new Person();
+    person1.name = "Роман";
+    person1.age = 50;
 
+
+
+}
+class Person {
+    String name;
+    int age;
+
+}
 
