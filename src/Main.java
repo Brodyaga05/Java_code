b14c5bec7f74d71d72e3ae5195fde5dd9711e401
Brodyaga05 <ulmart035@gmail.com>
2023-06-21 08:59:03 +0300

    array one

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-    /* int [] numbers = {1,2,3};
-        System.out.println(numbers[1]);
-        int [][] matrice = {{1,2,3,4,5},
-                            {32,4,5},
-                             {6,7,8}};
-        System.out.println(matrice[2][2]);
-        System.out.println(matrice[1][0]);
-        System.out.println(matrice[0][4]);*/
-        int[] number1 = new int[5];
-        String[][] strings = new String[2][3];
-        strings[0][1] = "Java Привет";
-        System.out.println(strings[0][1]);
+    int[][] matrice = {{1,2,3},
+                       {4,5,6},
+                       {7,8,9}};
+    for (int i = 0; i < matrice.length; i++){
+        for (int j = 0; j < matrice[i].length; j++){
+            System.out.println(matrice[i][j] + " ");
+        }
+        System.out.println();
+
+
+
+
+
+
+    }
 
 
 
