aabe9d04fb1a538263af08e635044f4cf8a43c51
Brodyaga05 <ulmart035@gmail.com>
2023-06-26 02:34:56 +0300

    String

--- src/type.java
+++ src/type.java
@@ -12,6 +12,9 @@ public class type {
         System.out.println(myInt);      // Outputs 9
         String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         System.out.println("The length of the txt string is: " + txt.length());
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());   // Outputs "hello world"
     }
 
 
