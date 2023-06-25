258b7563ffadad181666fee83030c192dbb402da
Brodyaga05 <ulmart035@gmail.com>
2023-06-26 01:54:28 +0300

    type double

--- src/type.java
+++ src/type.java
@@ -1,10 +1,15 @@
 public class type {
     public static void main(String[] args) {
-        int myInt = 9;
+       /* int myInt = 9;
         double myDouble = myInt; // Automatic casting: int to double
 
         System.out.println(myInt);      // Outputs 9
-        System.out.println(myDouble);   // Outputs 9.0
+        System.out.println(myDouble);   // Outputs 9.0*/
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
     }
 
 
