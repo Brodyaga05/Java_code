88031cd6324a1292058bce709f46a67b0393cc5f
Brodyaga05 <ulmart035@gmail.com>
2023-06-26 01:46:03 +0300

    type char

---
+++ src/type.java
@@ -0,0 +1,13 @@
+public class type {
+    public static void main(String[] args) {
+        char myVar1 = 65;
+        char myVar2 = 66;
+        char myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+    }
+
+
+}
+
