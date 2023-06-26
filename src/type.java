e2e670c12e49ecc7eec53adcb9e90437514dd332
Brodyaga05 <ulmart035@gmail.com>
2023-06-26 22:01:54 +0300

    String

--- src/type.java
+++ src/type.java
@@ -1,21 +1,10 @@
 public class type {
     public static void main(String[] args) {
-       /* int myInt = 9;
-        double myDouble = myInt; // Automatic casting: int to double
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        System.out.println(txt);
+        String txt2 = "It\'s alright.";
+   System.out.println(txt2);  }
 
-        System.out.println(myInt);      // Outputs 9
-        System.out.println(myDouble);   // Outputs 9.0*/
-        double myDouble = 9.78d;
-        int myInt = (int) myDouble; // Manual casting: double to int
-
-        System.out.println(myDouble);   // Outputs 9.78
-        System.out.println(myInt);      // Outputs 9
-        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.println("The length of the txt string is: " + txt.length());
-        String txt = "Hello World";
-        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
-        System.out.println(txt.toLowerCase());   // Outputs "hello world"
-    }
 
 
 }
