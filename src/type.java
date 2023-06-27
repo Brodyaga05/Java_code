1cadbd94d0b01ab73425a554e92a568a1f2ad2d2
Brodyaga05 <ulmart035@gmail.com>
2023-06-27 22:16:17 +0300

    math

--- src/type.java
+++ src/type.java
@@ -1,7 +1,7 @@
 public class type {
     public static void main(String[] args) {
         System.out.println(Math.max(5, 10));
-        System.out.println(Math.sqrt(25));
+        System.out.println(Math.abs(-4.7));
     }
 
 }
