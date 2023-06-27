ea78ae5b14ae8e5b96589c102e33c169b661020b
Brodyaga05 <ulmart035@gmail.com>
2023-06-27 22:15:00 +0300

    math

--- src/type.java
+++ src/type.java
@@ -1,11 +1,6 @@
 public class type {
     public static void main(String[] args) {
-        String txt = "We are the so-called \"Vikings\" from the north.";
-        System.out.println(txt);
-        String txt2 = "It\'s alright.";
-   System.out.println(txt2);
-    String txt3 = "The character \\ is called backslash.";
-     System.out.println(txt3);
+        System.out.println(Math.max(5, 10));
 
     }
 
