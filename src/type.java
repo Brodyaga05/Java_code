37ef0a08ef0e6d37024fa9df67a66b146e515577
Brodyaga05 <ulmart035@gmail.com>
2023-06-27 22:18:42 +0300

    math

--- src/type.java
+++ src/type.java
@@ -2,6 +2,8 @@ public class type {
     public static void main(String[] args) {
         System.out.println(Math.max(5, 10));
         System.out.println(Math.abs(-4.7));
+        int randomNum = (int)(Math.random() * 101);
+        System.out.println(randomNum);
     }
 
 }
