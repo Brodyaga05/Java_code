4c9a08b14bb9e6ef9f7153a2af1672bd6b81021b
Brodyaga05 <ulmart035@gmail.com>
2023-06-27 23:08:43 +0300

    random

--- src/type.java
+++ src/type.java
@@ -2,6 +2,7 @@ public class type {
     public static void main(String[] args) {
         String txt = "Hello Everybody";
         System.out.println(txt.indexOf("v"));
+        System.out.println(Math.random());
     }
 
 }
