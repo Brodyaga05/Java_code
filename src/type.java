d25a1d9e1f7f1a42f3514282147dfe0390b1d5c2
Brodyaga05 <ulmart035@gmail.com>
2023-06-27 22:29:21 +0300

    length

--- src/type.java
+++ src/type.java
@@ -1,9 +1,7 @@
 public class type {
     public static void main(String[] args) {
-        System.out.println(Math.max(5, 10));
-        System.out.println(Math.abs(-4.7));
-        int randomNum = (int)(Math.random() * 101);
-        System.out.println(randomNum);
+        String txt = "Hello";
+        System.out.println(txt.length());
     }
 
 }
