9a172430a08cd250265d88878f841ceda45cd625
Brodyaga05 <ulmart035@gmail.com>
2023-06-29 08:23:23 +0300

    method

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,15 +1,13 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
     Person person1 = new Person();
-    person1.name = "Роман";
+    person1.setName("Роман");
     person1.age = 50;
     Person person2 = new Person();
     person2.name = "Вова";
     person2.age = 20;
-    int years1 = person1.calculateYearsToRetirement();
-    int years2 = person2.calculateYearsToRetirement();
-    System.out.println("Первому человеку до пенсии: "+ years1 + " лет");
-    System.out.println("Первому человеку до пенсии: "+ years2 + " лет");
+
+
 
 }
 
