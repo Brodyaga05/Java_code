f88d3caf0b054b1b7ca943238d5fd3e6f28ee772
Brodyaga05 <ulmart035@gmail.com>
2023-06-30 08:11:20 +0300

    method

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -14,29 +14,26 @@ class Person {
    }
    public String getName(){
        return name;
-
    }
+   public void setAge(int userAge){
+       age = userAge;
+   }
+
 
     void setNameAndAge(String username, int userage){
         name = username;
         age = userage;
 
     }
-
-
-   int calculateYearsToRetirement(){
+    int calculateYearsToRetirement(){
         int years = 65-age;
         return years;
-
-
     }
     void speak(){
-
         System.out.println("Меня зовут " + name + ", мне " + age + " лет");
     }
     void  sayHello(){
         for (int i = 0; i < 2; i++)
         System.out.println("Hello");
-
     }
 }
