bf615e46c109499078816fb9b9855073e55459f7
Brodyaga05 <ulmart035@gmail.com>
2023-07-04 07:56:27 +0300

    number

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 public class MyNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-
+        System.out.println("Введите возраст");
+        int myAge = sc.nextInt();
+        int votingAge = 18;
+        if ( myAge >= votingAge){
+            System.out.println("Вы можете проголосовать");
+        } else {
+            System.out.println("Вы не можете проголосовать");
+        }
 
     }
-
-
-
-
 
     }
