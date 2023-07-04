0a2d1df220de2673b105d365f693f03f4e9f2a07
Brodyaga05 <ulmart035@gmail.com>
2023-07-04 07:34:10 +0300

    number

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -1,9 +1,13 @@
-
+import java.util.Scanner;
 public class MyNumber {
     public static void main(String[] args){
-        int cash = 16 / 5;
-        int remains = 16 % 5;
-        System.out.println(cash + " " + remains);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите целые числа: ");
+        int cash = sc.nextInt();
+        int remains = sc.nextInt();
+        System.out.println(cash / remains + " " + cash % remains);
+
+
     }
 
 
