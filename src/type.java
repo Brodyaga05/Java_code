2537a80cdcc4fb2bb4ef2b1d733429e7f08247f6
Brodyaga05 <ulmart035@gmail.com>
2023-07-16 17:55:47 +0300

    switch

--- src/type.java
+++ src/type.java
@@ -1,28 +1,21 @@
 public class type {
     public static void main(String[] args) {
-        int day = 4;
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
+        int day = 2;
+        switch (
+                day
+        ) {
+
+            case
+                    1:
                 System.out.println("Saturday");
                 break;
-            case 7:
+
+            case
+                    2:
                 System.out.println("Sunday");
-                break;
+
+                break
+                        ;
         }
 
     }}
