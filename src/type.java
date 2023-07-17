af473d7e18252a483e918d6dcdbec7e0d84df505
Brodyaga05 <ulmart035@gmail.com>
2023-07-17 21:10:51 +0300

    while

--- src/type.java
+++ src/type.java
@@ -1,21 +1,9 @@
 public class type {
     public static void main(String[] args) {
-        int day = 2;
-        switch (
-                day
-        ) {
-
-            case
-                    1:
-                System.out.println("Saturday");
-                break;
-
-            case
-                    2:
-                System.out.println("Sunday");
-
-                break
-                        ;
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
         }
 
     }}
