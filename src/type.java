1507560f360ad03f59e76bc75ad4150206311446
Brodyaga05 <ulmart035@gmail.com>
2023-07-18 21:34:10 +0300

    array

--- src/type.java
+++ src/type.java
@@ -2,6 +2,6 @@ public class type {
     public static void main(String[] args) {
         int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
         myNumbers[1][0] = 111;
-        System.out.println(myNumbers[1][0]);
+        System.out.println(myNumbers[1][2]);
     }}
 
