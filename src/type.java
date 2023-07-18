28cb8a3de2f06d6214cf4a2b9b2d933f67b6ddca
Brodyaga05 <ulmart035@gmail.com>
2023-07-18 21:19:40 +0300

    array

--- src/type.java
+++ src/type.java
@@ -1,8 +1,7 @@
 public class type {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        cars[0] = "Opel";
-        System.out.println(cars[0]);
+        String [] car = {"mazda", "volvo", "opel"};
+        System.out.println(car[0]);
 
 
 }}
