44f0da0c20028e21d48e771f8b85e232d4a6fd4a
Brodyaga05 <ulmart035@gmail.com>
2023-07-18 21:24:52 +0300

    array

--- src/type.java
+++ src/type.java
@@ -1,9 +1,10 @@
 public class type {
     public static void main(String[] args) {
-        String [] car = {"mazda", "volvo", "opel"};
-        for (int i = 0 ; i < car.length; i++)
-        System.out.println(car[i]);
+        String[] car = {"mazda", "volvo", "opel"};
+        for (String i : car) {
+            System.out.println(i);
 
 
-}}
+        }
+    }}
 
