23485c97957614694426983a898f996096f2cd22
Brodyaga05 <ulmart035@gmail.com>
2023-07-18 21:19:57 +0300

    array

--- src/type.java
+++ src/type.java
@@ -1,7 +1,7 @@
 public class type {
     public static void main(String[] args) {
         String [] car = {"mazda", "volvo", "opel"};
-        System.out.println(car[0]);
+        System.out.println(car.length);
 
 
 }}
