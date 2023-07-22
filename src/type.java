4d1e492be85c5dda0a2570196efdffd8476f2ecb
Brodyaga05 <ulmart035@gmail.com>
2023-07-22 14:32:55 +0300

    method

--- src/type.java
+++ src/type.java
@@ -4,7 +4,8 @@ public class type {
 
     }
     public static void main (String[] args) {
-        System.out.println(myMethod(5, 4));
+        int z = myMethod(5,6);
+        System.out.println(z);
     }
 
 
