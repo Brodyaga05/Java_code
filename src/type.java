301fdd55274924768ff6e6be62ef4c851008f55b
Brodyaga05 <ulmart035@gmail.com>
2023-07-22 14:00:57 +0300

    mothod

--- src/type.java
+++ src/type.java
@@ -1,10 +1,13 @@
 public class type {
-    public static void main(String[] args) {
-        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
-        for (int i = 0 ; i < myNumbers.length; ++i) {
-            for (int j = 0; j < myNumbers[i].length; ++j){
-                System.out.println(myNumbers[i][j]);
-            }
-        }
-    }}
+    static void myMethod() {
+        System.out.println("I just got executed!");
+
+    }
+    public static void main (String[] args) {
+        myMethod();
+    }
+
+
+
+}
 
