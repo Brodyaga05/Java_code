66e6ea0e9c1fd23efa394a19a1d3425a577f7416
Brodyaga05 <ulmart035@gmail.com>
2023-07-22 14:29:25 +0300

    method

--- src/type.java
+++ src/type.java
@@ -1,12 +1,10 @@
 public class type {
-    static void myMethod(String fname, int age) {
-        System.out.println(fname + " is " + age);
+    static int myMethod(int x) {
+        return 5 + x ;
 
     }
     public static void main (String[] args) {
-        myMethod("Liam", 5);
-        myMethod("Jenny", 6);
-        myMethod("Anja", 7);
+        System.out.println(myMethod(3));
     }
 
 
