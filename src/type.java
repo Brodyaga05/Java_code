b1a8c8a00825847e6ad3136565b272f8b31a0682
Brodyaga05 <ulmart035@gmail.com>
2023-07-22 14:30:36 +0300

    method

--- src/type.java
+++ src/type.java
@@ -1,6 +1,6 @@
 public class type {
-    static int myMethod(int x) {
-        return 5 + x ;
+    static int myMethod(int x, int y) {
+        return x + y;
 
     }
     public static void main (String[] args) {
