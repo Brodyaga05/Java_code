53b2601d26d4efd1320a3c1184f5a294cfafff19
Brodyaga05 <ulmart035@gmail.com>
2023-07-23 15:12:33 +0300

    class

--- src/type.java
+++ src/type.java
@@ -1,15 +1,18 @@
 public class type {
-    // Create a checkAge() method with an integer variable called age
-    public static void main(String[] args) {
-
-        // Code here CANNOT use x
-
-        int x = 100;
-
-        // Code here can use x
-        System.out.println(x);
-    }
+    int x = 5;
 }
+    // Create a checkAge() method with an integer variable called age
+
+
+
+
+
+
+
+
+
+
+
 
 
 
