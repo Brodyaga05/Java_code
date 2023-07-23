b7cfefdc2ac0bb85009c822fd2993b0595884ed7
Brodyaga05 <ulmart035@gmail.com>
2023-07-23 15:15:48 +0300

    class

---
+++ src/Type.java
@@ -0,0 +1,29 @@
+public class Type {
+    int x = 5;
+    public static void main(String[] args){
+        Type myObj = new Type();
+        System.out.println(myObj.x);
+
+
+
+
+
+    }
+}
+    // Create a checkAge() method with an integer variable called age
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
