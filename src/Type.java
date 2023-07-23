0354d256dcba1c9e9651ba3e3c6f9eff2a5484e0
Brodyaga05 <ulmart035@gmail.com>
2023-07-23 15:26:50 +0300

    class

--- src/Type.java
+++ src/Type.java
@@ -1,9 +1,9 @@
 public class Type {
-    int x = 5;
+   final int x = 5;
     int y = 3;
     public static void main(String[] args) {
         Type myObj = new Type();
-        myObj.x = 777;
+        myObj.y = 777;
         System.out.println(myObj.x + myObj.y);
     }
 
