71bf75f794424995362e82bba44f99e361ecf286
Brodyaga05 <ulmart035@gmail.com>
2023-07-23 15:20:46 +0300

    class

--- src/Type.java
+++ src/Type.java
@@ -1,17 +1,12 @@
 public class Type {
     int x = 5;
-    public static void main(String[] args){
-        Type myObj1 = new Type();
-        Type myObj2 = new Type();
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.x);
 
 
 
 
 
 
-    }
+
 }
 
 
---
+++ src/Type2.java
@@ -0,0 +1,10 @@
+public class Type2 {
+public static void main(String[] args) {
+
+    Type myObj = new Type();
+    System.out.println(myObj.x);
+}
+
+
+
+}
