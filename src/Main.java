58879bf20081682ac98e259305714c5732b231ee
Brodyaga05 <ulmart035@gmail.com>
2023-07-24 23:05:08 +0300

    class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
     public static void main(String[] args) {
+        myMethod();
     }
-
-
-
-
-
-
-    }
+}
 
 
 
