17c82528673b1e72ec3e76174e3610968524614b
Brodyaga05 <ulmart035@gmail.com>
2023-08-01 20:41:29 +0300

    class

--- src/Type2.java
+++ src/Type2.java
@@ -1,10 +1,8 @@
 public class Type2 {
-public static void main(String[] args) {
-
-    Type myObj = new Type();
-    System.out.println(myObj.x);
-}
-
-
-
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
 }
